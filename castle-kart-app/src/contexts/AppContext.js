7b77d28ef5796abd5d9d9fc94ab348516189a4c9
Sweta <swetabisht4b@outlook.com>
2023-06-06 19:53:48 +0530

    Update AppContext.js

--- castle-kart-app/src/contexts/AppContext.js
+++ castle-kart-app/src/contexts/AppContext.js
@@ -23,8 +23,9 @@ export default function AppProvider({ children }) {
     const [userData, setUserData] = useState({});
     const[showSidenav, setShowSidenav] = useState(false)
     const navigate = useNavigate();
+    const [isSelected, setIsSelected] = useState(false);
    
-    
+
 
 
     useEffect(() => {
@@ -182,7 +183,9 @@ export default function AppProvider({ children }) {
                 handleSortingChange,
                 showSearchedProduct,
                 showSidenav,
-                toggleSideNav
+                toggleSideNav,
+                isSelected,
+                setIsSelected
             }}
         >
             {children}
